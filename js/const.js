fb174cdf5c3c5866cd484c9792e97083e58cabec
SoloShot376 <soloshot3769@gmail.com>
2022-07-05 01:12:12 +0200

    const.js addresses reference file added

---
+++ js/const.js
@@ -0,0 +1,16 @@
+const HEDERA = {
+    TESTNET: "0.0.46810017",
+    PREVIEWNET: "0.0.3882",
+    MAINNET: "0.0.1",
+};
+
+const ETHEREUM = {
+    RINKEBY: "0x785C507E0E46c7562320677705975fff1b04F75D",
+    SEPOLIA: "0x0",
+    GOERLI:  "0x0",
+    MAINNET: "0x0",
+}
+
+// SOLANA soon
+
+export {HEDERA, ETHEREUM};
